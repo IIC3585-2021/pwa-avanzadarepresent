048061cc659baf681ad6fc0385680565dfd88daa
entamburini <entamburini@uc.cl>
2021-05-23 21:15:33 -0400

    Beautify content

--- frontend/js/feed.js
+++ frontend/js/feed.js
@@ -19,7 +19,8 @@ const sendLikes = function(postId) {
 function generateCardHTML(key, post) {
 
     return `
-    <div class="card" style="width: 18rem;">
+    </br>
+    <div class="card">
         <div class="card-body">
             <h5 class="card-title">${post.title}</h5>
             <h6 class="card-subtitle mb-2 text-muted">${post.author}</h6>
